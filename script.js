a3bc65dd5ea22a49eaff814c10d0f383852d018b
Joseu-19 <95296240+Joseu-19@users.noreply.github.com>
2024-08-17 12:51:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let screenWidth = document.body.offsetWidth;
+let screenHeight = document.body.offsetHeight;
+
+let noBtn = document.querySelector('.noBtn');
+let yesBtn = document.querySelector('.yesBtn');
+let text = document.querySelector('h1');
+let picture = document.querySelector('#avatar');
+function move(){
+    let flag = false;
+
+    while(!flag){
+        let randX = Math.random() * screenWidth;
+        let randY = Math.random() * screenHeight;
+
+        noBtn.style.left = randX + 'px';
+        noBtn.style.top = randY + 'px';
+
+        flag  = true;
+    }
+}
+
+noBtn.addEventListener('mouseenter', ()=>{
+    move();
+});
+
+yesBtn.addEventListener('click', ()=>{
+
+    text.textContent = 'yayyyy ';
+    noBtn.style.display = 'none';
+    avatar.src = './images/happy.webp';
+
+
+});
